75095b4f10cdc173dc044622d1d04a55da15d05b
Adonai <gn.vicidomini@gmail.com>
2024-06-27 19:20:03 +0200

    refinements

--- exercises/lcel.py
+++ exercises/lcel.py
@@ -9,15 +9,15 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 def exercise():
     load_dotenv()
     prompt = """Tell a joke about {topic}"""
-    template = PromptTemplate(input_variables=["topic"],template=prompt)
+    template = PromptTemplate(input_variables=["topic"], template=prompt)
     llm = ChatGroq(temperature=1, model=os.getenv("MODEL_GROQ"))
     """
     DOES NOT WORK
     llm = HuggingFaceEndpoint(
-        repo_id=os.getenv("MODEL_HUGGINGFACE"),
         task="text-generation",
         max_new_tokens=512,
         do_sample=False,
+        repo_id=os.getenv("MODEL_HUGGINGFACE"),
         repetition_penalty=1.03,
     )
     chat_model = ChatHuggingFace(llm=llm)
